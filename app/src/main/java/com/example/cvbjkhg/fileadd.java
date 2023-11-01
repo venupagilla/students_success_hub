250fa61b1ab1632e227dcd2a50a2bd2fe76449af
venu pagilla <94222926+venupagilla@users.noreply.github.com>
2023-11-01 23:20:48 +0530

    wrote the code for uploading the pdf files

--- app/src/main/java/com/example/cvbjkhg/fileadd.java
+++ app/src/main/java/com/example/cvbjkhg/fileadd.java
@@ -1,13 +1,28 @@
 package com.example.cvbjkhg;
-import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
-import android.widget.ArrayAdapter;
-import android.widget.AdapterView;
-import android.view.View;
 import android.widget.Toast;
-import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 public class fileadd extends AppCompatActivity {
 
@@ -15,6 +30,11 @@ public class fileadd extends AppCompatActivity {
     private String selectedbatch;
     private String selectedexamtype;
     private String selectedsub;
+
+
+
+    StorageReference storageReference;
+    DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -113,14 +133,71 @@ public class fileadd extends AppCompatActivity {
         // You can populate sub_spinner based on selectedItem2 here
         populateSubSpinner();
 
+        //Database
+        storageReference= FirebaseStorage.getInstance().getReference();
+        databaseReference= FirebaseDatabase.getInstance().getReference("uploads");
+
 
         //code for uploading using upload button
         upload_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                selectfiles();
             }
         });
+    }
+
+    private void selectfiles(){
+        Intent intent=new Intent();
+        intent.setType("application/pdf");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent,"select PDf file.."),1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode==1 && resultCode==RESULT_OK && data!=null && data.getData()!=null){
+
+            Uploadfiles(data.getData());
+        }
+    }
+
+    private void Uploadfiles(Uri data) {
+        final ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setTitle("uploading..");
+        progressDialog.show();
+
+        StorageReference reference = storageReference.child("uploads/"+System.currentTimeMillis()+".pdf");
+        reference.putFile(data)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+
+                        Task<Uri> uriTask=taskSnapshot.getStorage().getDownloadUrl();
+                        while (!uriTask.isComplete());
+                        Uri url=uriTask.getResult();
+
+                        String pdfname= selectedbatch + " " + selectedsem + " " + selectedexamtype + " " + selectedsub;
+
+                        pdfClass pdfClass = new pdfClass(pdfname,url.toString());
+                        databaseReference.child(databaseReference.push().getKey()).setValue(pdfClass);
+
+                        Toast.makeText(fileadd.this, "File uploaded successfully", Toast.LENGTH_SHORT).show();
+
+                        progressDialog.dismiss();
+
+                    }
+                }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onProgress(@NonNull UploadTask.TaskSnapshot snapshot) {
+                        double progress = (100.0* snapshot.getBytesTransferred())/snapshot.getTotalByteCount();
+                        progressDialog.setMessage("uploaded:"+(int)progress+"%");
+
+                    }
+                });
     }
 
     private void populateSubSpinner() {
